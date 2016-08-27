67324defe6832c68c0db081080c9c14ad09d66b6
Scheps <loic.kempf@gmail.com>
2016-08-27 20:25:52 +0200

    drop + recup lettre

--- Assets/Scripts/Player/Dash.cs
+++ Assets/Scripts/Player/Dash.cs
@@ -6,10 +6,17 @@ public class Dash : MonoBehaviour {
 	public float 			dashTime;
 	public LeanTweenType 	tweenType;
 
-	Vector2 heading;
-	Vector2 destination;
+	private	Vector2 		heading;
+	private	Vector2 		destination;
+	private bool 			hasDroppedLetter;
+	private bool			isInvincible;
+
+	private SpriteRenderer	sprite;
+
+
 	// Use this for initialization
 	void Start () {
+		sprite = this.GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -35,14 +42,42 @@ public class Dash : MonoBehaviour {
 
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Projectile") {
-			Debug.Log ("BOOM");
-		}
+		if (!isInvincible && other.tag == "Projectile") {
+			if (!hasDroppedLetter) {
+				
+				DropLetter ();
+				isInvincible = true;
 
+				StartCoroutine (DoBlinks (10, 0.1f));
+		
+				//Invoke ("BecomeVincible", 0.5f);
+				Debug.Log ("BOOM");
+			} else {
+				Debug.Log ("GAME OVER");
+			}
+		} else if (other.tag == "Letter") {
+			hasDroppedLetter = false;
+			Destroy (other.gameObject);
+		}
 	}
+		
 
 	void DropLetter()
 	{
+		hasDroppedLetter = true;
+		Instantiate (PrefabManager.instance.letter,this.transform.position, Quaternion.identity);
+	}
 
+	IEnumerator DoBlinks(int numBlinks, float seconds) {
+		for (int i=0; i<numBlinks*2; i++) {
+
+				sprite.enabled = !sprite.enabled;
+			//wait for a bit
+			yield return new WaitForSeconds(seconds);
+		}
+
+		//make sure renderer is enabled when we exit
+		sprite.enabled=true;
+		isInvincible = false;
 	}
 }
