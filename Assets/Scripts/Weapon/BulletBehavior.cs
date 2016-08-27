8f5df29ac4868df0527cacbcfadec6d2b704de09
Lacroute <procureur.clement@gmail.com>
2016-08-27 19:52:02 +0200

    DCA init & inheritance projectile behavior

---
+++ Assets/Scripts/Weapon/BulletBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehavior : Projectile {
+	
+	// Variables pour Munition 
+	public bool projectCanFollow;
+
+
+	public void Update () {
+		if(projectCanFollow){
+			StartCoroutine ("FollowPlayer");
+		}
+		else SraightMovement();
+	}
+
+
+	public void SraightMovement(){
+		transform.Translate (Vector3.forward * Time.deltaTime * projectileSpeed);
+	}
+
+
+	public IEnumerator FollowPlayer(){
+		transform.LookAt (target.transform);
+		transform.Translate (Vector3.forward * Time.deltaTime * projectileSpeed);
+
+		yield return new WaitForSeconds (timeBeforeDestruction);
+		Destruct ();
+	}
+}
--- Assets/Scripts/Weapon/Projectile.cs
+++ Assets/Scripts/Weapon/Projectile.cs
@@ -1,66 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour {
+public abstract class Projectile : MonoBehaviour {
 	public float projectileSpeed;
 	public float projectilSize;
 	public Material projectileMaterial;
 
 	// Variables pour Munition 
-	public bool projectCanFollow;
 	public float timeBeforeDestruction;
-	public GameObject projectileTarget;
+	public GameObject target;
 
 
-	void Awake(){
-		projectileTarget = GameObject.FindGameObjectWithTag ("Player");
+	public virtual void Awake(){
+		target = GameObject.FindGameObjectWithTag ("Player");
 		SetProjectilSize ();
 	}
 
 
-	void Update () {
-		if(projectCanFollow){
-			StartCoroutine ("FollowPlayer");
-		}
-		else SraightMovement();
-	}
-
-
-	void SetProjectilSize(){
-		// Set up SIZE
+	public virtual void SetProjectilSize() {
 		transform.localScale = new Vector3(projectilSize,projectilSize,projectilSize);
 	}
 
 
-	void SraightMovement(){
-		transform.Translate (Vector3.forward * Time.deltaTime * projectileSpeed);
-	}
-
-
-	IEnumerator FollowPlayer(){
-		transform.LookAt (projectileTarget.transform);
-		transform.Translate (Vector3.forward * Time.deltaTime * projectileSpeed);
-
-		yield return new WaitForSeconds (timeBeforeDestruction);
-		Destruct ();
-	}
-
-
-	void OnTriggerEnter(Collider other){
+	public virtual void OnTriggerEnter(Collider other){
 		if (other.tag == "Player") {
-			Debug.Log ("Joueur touché");
+			Debug.LogWarning ("PLAYER HIT");
 			Destruct ();
 		}
 	}
 
 
-	void OnBecameInvisible() {
-		Debug.Log ("CIAO");
+	public virtual void OnBecameInvisible() {
 		Destruct ();
 	}
 
 
-	public void Destruct() {
+	public virtual void Destruct() {
 		Destroy (gameObject);
 	}
 }
