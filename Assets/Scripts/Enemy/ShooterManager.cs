ef99e779c833281a95892282c8661c167ad5bd6e
francoiswas <francois.cavalin@wearesocial.fr>
2016-08-27 21:39:50 +0200

    Added Skybox / Shitty bird and clouds + FUCKIN MISSILE MAAAAN // A fixer : orientation des missiles // Scene Art

--- Assets/Scripts/Enemy/ShooterManager.cs
+++ Assets/Scripts/Enemy/ShooterManager.cs
@@ -62,7 +62,7 @@ public class ShooterManager : MonoBehaviour {
 			);
 		}
 
-		clone.transform.position = new Vector3(spawnPosition.x, spawnPosition.y, 1);
+		clone.transform.position = new Vector3(spawnPosition.x, spawnPosition.y, 0);
 	}
 
 
