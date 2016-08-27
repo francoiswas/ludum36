54703199148077afd73a7a57ae73dd8b8869f92b
francoiswas <francois.cavalin@wearesocial.fr>
2016-08-27 14:15:59 +0200

    Francis Folder : Turret with autou AIM (Set up target in editor// Set up Projectile in Editor)

---
+++ Assets/Sandbox/Francis/Turret.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+	public GameObject enemyTarget;
+	public GameObject projectilePrefab;
+
+	IEnumerator FireProjectile(){
+		GameObject projectile = Instantiate (projectilePrefab, transform.position, Quaternion.identity) as GameObject;
+		projectile.transform.rotation = transform.rotation;
+		yield return new WaitForSeconds(1F);
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AimTarget ();
+
+		if(Input.GetKeyDown("space")){
+			StartCoroutine ("FireProjectile");
+		}
+
+	}
+		
+
+	void AimTarget(){
+		transform.LookAt (enemyTarget.transform.position);
+	}
+}
