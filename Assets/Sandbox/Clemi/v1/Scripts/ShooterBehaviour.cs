42f8c003276da9d067bbf80b6a12a04826843fb0
Lacroute <procureur.clement@gmail.com>
2016-08-27 16:59:09 +0200

    Burst with promise instantiation

--- Assets/Sandbox/Clemi/v1/Scripts/ShooterBehaviour.cs
+++ Assets/Sandbox/Clemi/v1/Scripts/ShooterBehaviour.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using RSG;
 
 public class ShooterBehaviour : MonoBehaviour {
 
@@ -10,15 +12,22 @@ public class ShooterBehaviour : MonoBehaviour {
 	public GameObject followingBullet;
 	public bool canFollow;
 
+	[Range (1, 10)]
+	public int bulletPerBurst = 3;
+	[Range (0, 10)]
+	public float bulletFreqency = 1f;
+	[Range (0, 10)]
+	public float burstFreqency = 3f;
+
 
 	// Use this for initialization
 	void Start () {
 		enemyTarget = GameObject.FindGameObjectWithTag ("Player");
 
 		// Placeholder for dynamic instantiation.
-		canFollow = Random.Range (0, 2) >= 1 ? true : false;
+		canFollow = UnityEngine.Random.Range (0, 2) >= 1 ? true : false;
 
-//		canFollow = true;
+		InvokeRepeating ("Shoot", 1, burstFreqency);
 	}
 
 
@@ -31,15 +40,44 @@ public class ShooterBehaviour : MonoBehaviour {
 			Debug.Log ("PROJECTILE CLASSIC");
 			bullet = classicBullet;
 		}
-	
-		if (Input.GetMouseButtonDown (0)) {
-			shoot ();
-		}
+	}
+
+	/// <summary>
+	/// Main shooting method.
+	/// </summary>
+	void Shoot() {
+		Shoot (bulletPerBurst);
 	}
 
 
-	void shoot(){
+	/// <summary>
+	/// Recursive shooting method. Instantiate nbBullet via a Coroutine.
+	/// </summary>
+	/// <param name="nbBullet">Nb bullet.</param>
+	void Shoot(int nbBullet){
+		Promise promise = new Promise((resolve, reject) => {
+			nbBullet--;
+			StartCoroutine(LaunchBulletCoroutine(resolve, reject));
+		});
+
+		promise.Done( () => {
+			if (nbBullet > 0) Shoot(nbBullet);
+		});
+	}
+
+
+	/// <summary>
+	/// Launch the bullet coroutine, instantiate a bullet after a delay.
+	/// </summary>
+	/// <returns>The bullet coroutine.</returns>
+	/// <param name="resolve">Resolve.</param>
+	/// <param name="reject">Reject.</param>
+	private IEnumerator LaunchBulletCoroutine(Action resolve, Action<Exception> reject) {
+		yield return new WaitForSeconds(bulletFreqency);
+
 		GameObject clone = Instantiate (bullet, transform.position, Quaternion.identity) as GameObject;
 		clone.transform.rotation = transform.rotation;
+
+		resolve();
 	}
 }
