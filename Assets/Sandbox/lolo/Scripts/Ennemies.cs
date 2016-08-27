699f1a5c8936b8adf6db43accd02f4ea9445eab4
Scheps <loic.kempf@gmail.com>
2016-08-27 17:34:47 +0200

    dash + mecanique vertical

---
+++ Assets/Sandbox/lolo/Scripts/Ennemies.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ennemies : MonoBehaviour {
+	public GameObject prefabEnnemy;
+	void Start () {
+		InvokeRepeating ("spawn", 1, 1);
+	}
+	
+	void spawn()
+	{
+		Instantiate (prefabEnnemy);
+
+	}
+}
---
+++ Assets/Sandbox/lolo/Scripts/World.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class World : MonoBehaviour {
+	public static World instance = null;
+
+	int score;
+
+	public Text textScore;
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GameOver()
+	{
+		score = 0;
+		updateScore ();
+		Ennemy[] ennemies=GameObject.FindObjectsOfType<Ennemy> ();
+		for (int i = 0; i < ennemies.Length; i++) {
+			Destroy (ennemies [i].gameObject);
+		}
+		Coin[] coins=GameObject.FindObjectsOfType<Coin> ();
+		for (int i = 0; i < coins.Length; i++) {
+			Destroy (coins [i].gameObject);
+		}
+	}
+
+
+	public void AddScore()
+	{
+		score++;
+		updateScore ();
+	}
+
+	void updateScore()
+	{
+		textScore.text = score.ToString ();
+	}
+}
