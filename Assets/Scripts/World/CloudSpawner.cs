c86e41efdfe14fdfa0a7b009de22bd33943b039c
Lacroute <procureur.clement@gmail.com>
2016-08-28 19:46:54 +0200

    make prefac of everything MA GUEULE

---
+++ Assets/Scripts/World/CloudSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudSpawner : MonoBehaviour {
+	public GameObject Cloudprefab1;
+	public GameObject CloudPrefab2;
+	public Vector3[] CloudSpawn;
+
+	void Awake () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
